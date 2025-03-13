f5b50413b6afd26f4261b6ea85af3c73e22cfb61
Emiliaikpa <emiliaaikpa@gmail.com>
2025-03-13 03:00:17 +0100

    003_numeric_data.py

---
+++ 003_numeric_data.py
@@ -0,0 +1,33 @@
+num1 =10
+num2 =5.5
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 // num2)
+print(num1 ** num2)
+print(num1 % num2)
+
+
+print((10 + 3) * 2 ** 2 / 4 - 1)
+
+
+print(abs(-15))
+
+
+print(round(num2, 1))
+print(round(num2, 2))
+
+
+print(num1 == num2)
+print(num1 != num2)
+print(num1 > num2)
+print(num1 < num2)
+print(num1 >= num2)
+print(num1 <= num2)
+
+
+print(int(num2))
+print(float(num1))
+print(int("20"))
