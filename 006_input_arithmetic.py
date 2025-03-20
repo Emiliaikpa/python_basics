36e0bcfc7413a365ec4462f394b0016414cae860
Emiliaikpa <emiliaaikpa@gmail.com>
2025-03-20 23:00:28 +0100

    006_input_arithmetic.py

--- 006_input_arithmetic.py
+++ 006_input_arithmetic.py
@@ -1,7 +1,7 @@
 x = input("enter 1st number")
 y = input("enter 2nd number")
 
-x = int(input("enter ist number"))
+x = int(input("enter 1st number"))
 y = float(input("enter 2nd number"))
 
 z = x + y
