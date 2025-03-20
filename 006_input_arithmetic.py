4c0e04cd259c32628b5d602637e004455f6ac6ae
Emiliaikpa <emiliaaikpa@gmail.com>
2025-03-20 22:29:16 +0100

    Emilia,if you make mistake again, i will give you money

---
+++ 006_input_arithmetic.py
@@ -0,0 +1,17 @@
+x = input("enter 1st number")
+y = input("enter 2nd number")
+
+x = int(input("enter ist number"))
+y = float(input("enter 2nd number"))
+
+z = x + y
+print(z)
+
+z = x - y
+print(z)
+
+z = x * y
+print(z)
+
+z = x / y
+print(z)
