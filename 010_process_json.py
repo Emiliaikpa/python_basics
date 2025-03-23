04fd4dfd4627dc3d5fa850374c464f49fd070b28
Emiliaikpa <emiliaaikpa@gmail.com>
2025-03-23 22:41:27 +0100

    010_process_json.py

--- 010_process_json.py
+++ 010_process_json.py
@@ -3,34 +3,34 @@ import json
 with open("009_data.json", "r") as file:
     car = json.load(file)
 
-    print(car["brand"])
+print(car["brand"])
 
-    print(car.get("year"))
+print(car.get("year"))
 
-    print(car["features"])
+print(car["features"])
 
-    print (car["engine"]["horsepower"])
+print (car["engine"]["horsepower"])
     
-    car["year"] = 2025
+car["year"] = 2025
 
-    car["speed"] = "180 km/h"
+car["speed"] = "180 km/h"
 
-    car.update({"country": "Japan"})
+car.update({"country": "Japan"})
 
-    del car["color"]
+del car["color"]
 
-    car.pop("model")
+car.pop("model")
 
 with open("011_new_data.json", "w") as new_file:
     json.dump(car, new_file)
 
-    print(len(car))
+print(len(car))
 
-    print(car.keys())
+print(car.keys())
 
-    print(car.values())
+print(car.values())
 
-    print(car.items())
+print(car.items())
 
 
 for key in car.keys():
