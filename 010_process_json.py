334a8ed77e78131aae98204ef8e671a50925daff
Emiliaikpa <emiliaaikpa@gmail.com>
2025-03-23 20:55:46 +0100

    010_process_json.py

---
+++ 010_process_json.py
@@ -0,0 +1,43 @@
+import json
+
+with open("009_data.json", "r") as file:
+    car = json.load(file)
+
+    print(car["brand"])
+
+    print(car.get("year"))
+
+    print(car["features"])
+
+    print (car["engine"]["horsepower"])
+    
+    car["year"] = 2025
+
+    car["speed"] = "180 km/h"
+
+    car.update({"country": "Japan"})
+
+    del car["color"]
+
+    car.pop("model")
+
+with open("011_new_data.json", "w") as new_file:
+    json.dump(car, new_file)
+
+    print(len(car))
+
+    print(car.keys())
+
+    print(car.values())
+
+    print(car.items())
+
+
+for key in car.keys():
+    print(key)
+
+for key, value in car.items():
+    print(key, value)
+
+
+
