a764519c6f4ff5f025f84e7c9c2bc1f4ec611650
Emiliaikpa <emiliaaikpa@gmail.com>
2025-04-04 13:58:43 +0100

    026_function.py

---
+++ 026_function.py
@@ -0,0 +1,20 @@
+def greet_user():
+    print("hello, welcome to python programing!")
+
+greet_user()
+
+
+def add_numbers(a, b):
+    c = a + b
+    print(c)
+
+add_numbers(10, 5)
+
+
+def multiply_numbers(a, b):
+    c = a * b
+    return c
+
+result = multiply_numbers(6, 4)
+
+print(result)
